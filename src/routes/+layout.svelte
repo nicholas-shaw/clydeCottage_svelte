e7b516f641da3f1025c3d6f607a2e6a5f2fd9953
Nicholas Shaw <nicholasshaw@Nicholass-Mac-mini.local>
2023-12-07 07:51:04 +1100

    Placeholder text and layout added - not final.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<header>
+    <div class="logoScript">CC</div>
+    <h1>Clyde cottage</h1>
+    <hr />
+</header>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jost&family=Lato:wght@100;300;400;700&family=Waterfall&display=swap');
+</style>
+
+<script>
+  import '../app.css'
+</script>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
-<h1>Clyde cottage</h1>
-<p>Discover the charm of Tasmania's White Beach in our cozy, picturesque cottage, nestled in the heart of the Tasman Peninsula. This is the ideal spot for those seeking the perfect blend of tranquility and adventure. White Beach, renowned for its crystal-clear waters and pristine sands, is a haven for beach lovers. Meanwhile, the nearby Tasman National Park offers breathtaking hiking trails for nature enthusiasts. This cottage is not just an accommodation, it's a gateway to the best of Tasmania's natural beauty.</p>
-<p>The cottage is conveniently located, providing easy access to Tasmania's must-visit attractions. Just a short drive away, you'll find the historic Port Arthur site, offering a fascinating insight into Australia's convict history. For those seeking thrill, the Tasman Island Cruises provide an unforgettable journey around the rugged coastline. Foodies will delight in the local seafood at the nearby fishing village of Dunalley, while wine connoisseurs can explore the award-winning vineyards in the region.</p>
-<p>So why wait? Book your stay at our cozy cottage in White Beach, Tasmania, and immerse yourself in the unique experiences the Tasman Peninsula has to offer. Whether you're a history buff, a nature lover, a food and wine enthusiast, or simply seeking a peaceful getaway, this cottage is the perfect accommodation choice. Experience Tasmania in its most authentic form, right from the comfort of our homely cottage.</p>
+<div class='heroImage'><img src="https://a0.muscache.com/im/pictures/miso/Hosting-51540661/original/33edad68-109a-40c1-b882-1af899df7114.jpeg?im_w=960" alt="Clyde Cottage exterior view showing the front verandah"></div>
+<div class="articleContainer">
+    <p>
+        Discover the charm of Tasmania's White Beach in our cozy, picturesque cottage, nestled in the heart of the Tasman Peninsula. This is the ideal spot for those seeking the perfect blend of tranquility and adventure. White Beach, renowned for its crystal-clear waters and pristine sands, is a haven for beach lovers. Meanwhile, the nearby Tasman National Park offers breathtaking hiking trails for nature enthusiasts. This cottage is not just an accommodation, it's a gateway to the best of Tasmania's natural beauty.
+    </p>
+    <p>
+        The cottage is conveniently located, providing easy access to Tasmania's must-visit attractions. Just a short drive away, you'll find the historic Port Arthur site, offering a fascinating insight into Australia's convict history. For those seeking thrill, the Tasman Island Cruises provide an unforgettable journey around the rugged coastline. Foodies will delight in the local seafood at the nearby fishing village of Dunalley, while wine connoisseurs can explore the award-winning vineyards in the region.
+    </p>
+    <p>
+        So why wait? Book your stay at our cozy cottage in White Beach, Tasmania, and immerse yourself in the unique experiences the Tasman Peninsula has to offer. Whether you're a history buff, a nature lover, a food and wine enthusiast, or simply seeking a peaceful getaway, this cottage is the perfect accommodation choice. Experience Tasmania in its most authentic form, right from the comfort of our homely cottage.
+    </p>
+</div>
